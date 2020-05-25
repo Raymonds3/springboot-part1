a610492ddf685dd4d647e62723e3dab5c677dc52
Raymond Serekwane <raymonds338@gmail.com>
2020-05-26 00:19:36 +0200

    commit update

--- src/test/java/com/example/springbootpart1/Springbootpart1ApplicationTests.java
+++ src/test/java/com/example/springbootpart1/Springbootpart1ApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.springbootpart1;
 
+import com.example.springbootpart1.services.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Springbootpart1ApplicationTests {
 
+	@Autowired
+	private UserServiceImpl userService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void addUser() {
+		assertThat(this.userService.addUser(5, "Simcar", "Mahlangu"))
+				.contains("Simcar Mahlangu was Entered");
+//		assertThat(this.userService.addUser(5, "Simcar", "Mahlangu")
+//				.contains("Simcar Mahla was Entered"));
 	}
 
 }
